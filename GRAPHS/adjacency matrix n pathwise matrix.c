524da1fcfc201bb4e39d74aa4341ea5caed05768
Khushi Chavada <khushichavada0112@gmail.com>
2020-06-10 11:26:18 +0530

    Update adjacency matrix n pathwise matrix.c

--- GRAPHS/adjacency matrix n pathwise matrix.c
+++ GRAPHS/adjacency matrix n pathwise matrix.c
@@ -6,39 +6,40 @@ int main()
     int A[5][5],P[5][5];
     int i,j,k;
 
-    for(i=1;i<=5;i++)
+    for(i=0;i<5;i++)
     {
-        for(j=1;j<=5;j++)
-            {A[i][j]=0;}
+        for(j=0;j<5;j++)
+            {
+              A[i][j]=0;
+            }
+   }
 
-    }
-
-    A[1][1]=1;
+    A[0][0]=1;
+    A[0][1]=1;
+    A[0][3]=1;
     A[1][2]=1;
-    A[1][4]=1;
-    A[2][3]=1;
-    A[3][5]=1;
-    A[4][3]=1;
+    A[2][4]=1;
+    A[3][2]=1;
 
     printf("PRINTING ADJACENCY MATRIX:\n");
-    for(i=1;i<=5;i++)
+    for(i=0;i<5;i++)
     {
-        for(j=1;j<=5;j++)
+        for(j=0;j<5;j++)
             {printf("%d",A[i][j]);
             P[i][j]=A[i][j];}
             printf("\n");
 
     }
-    for(k=1;k<=5;k++)
-        for(i=1;i<=5;i++)
-        for(j=1;j<=5;j++)
+    for(k=0;k<5;k++)
+        for(i=0;i<5;i++)
+        for(j=0;j<5;j++)
         P[i][j]=OR(P[i][j],AND(P[i][k],P[k][j]));
 
     printf("\n\nPATH MATRIX\n\n");
 
-for(i=1;i<=5;i++)
+for(i=0;i<5;i++)
 {
-    for(j=1;j<=5;j++)
+    for(j=0;j<5;j++)
     printf("%d",P[i][j]);
    printf("\n");
 }
